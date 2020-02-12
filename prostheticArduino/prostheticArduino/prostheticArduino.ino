7aa7ad4446aef13a4cba2ad6b9179427d6a4565e
grboehringer <52945136+grboehringer@users.noreply.github.com>
2020-02-11 19:28:11 -0600

    Add files via upload

---
+++ prostheticArduino/prostheticArduino/prostheticArduino.ino
@@ -0,0 +1,14 @@
+int flexiForcePin = A0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int flexiForceReading = analogRead(flexiForcePin);
+
+  Serial.println(flexiForceReading);
+  delay(250);
+
+}
