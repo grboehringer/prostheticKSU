c74c6e62c2d2a2f50c4800a33621bbcd8bda1a80
grboehringer <g2boehringer@gmail.com>
2020-02-17 17:17:40 -0600

    added ArduinoBLE.h and added general code for bluetooth detection. Need to add uuid for the arduino.

--- prostheticKSU/arduinoCode/arduinoCode.ino
+++ prostheticKSU/arduinoCode/arduinoCode.ino
@@ -1,18 +1,42 @@
-#include <CurieBLE>
+#include <ArduinoBLE.h>
+
+BLEService boaService("");//add uuid
+BLEUnsignedCharCharacteristic boaChar("", BLERead | BLENotify);//add uuid
 
 int flexiForcePin = A0;
 
 void setup() {
   Serial.begin(9600);
-  stuff
+  if (!BLE.begin()){
+    Serial.println("Starting BLE failed.");
+
+    while(1);
+  }
+  BLE.setLocalName("BoaMonitor");
+  BLE.setAdvertisedService(boaService);
+  boaService.addCharacteristic(boaChar);
+  BLE.addService(boaService);
+
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
+
 
 }
 
 void loop() {
-  int flexiForceReading = analogRead(flexiForcePin);
+  BLEDevice central = BLE.central();
 
-  Serial.println(flexiForceReading);
-  delay(250);
+  if (central){
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+  }
+  
+  while(central.connected()){
+    int flexiForceReading = analogRead(flexiForcePin);
+
+    Serial.println(flexiForceReading);
+    delay(250);
+  }
 
 }
-https://github.com/grboehringer/prostheticArduino.git
+//https://github.com/grboehringer/prostheticArduino.git
